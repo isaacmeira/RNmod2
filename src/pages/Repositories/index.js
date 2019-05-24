bf67f3eb4e3053a7fadab74225560b3058c3a853
isaac meira <fspp@outlook.com>
2019-05-24 00:15:42 -0300

    .

--- src/pages/Repositories/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { View } from "react-native";
-
-// import { Container } from './styles';
-
-const Repositories = () => <View />;
-
-export default Repositories;
